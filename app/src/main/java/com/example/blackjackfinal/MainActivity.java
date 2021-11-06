dd787ac6329fc28f3f627bc027ab125e27ea679b
hcbryant9 <bryanthc@vcu.edu>
2021-11-05 20:17:58 -0400

    second commit (hb) -> added a button to the add funds and created a new "add funds" activity that it takes you to when you click the button. There was a small bug when moving around the button but i found a short term fix for now

--- app/src/main/java/com/example/blackjackfinal/MainActivity.java
+++ app/src/main/java/com/example/blackjackfinal/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.blackjackfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,20 +11,32 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int balance = 0;
+    private Button playBTN;
+    private Button addFundsBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button playBTN = findViewById(R.id.playbtn);
-        playBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Log.i("My App", "This is a magic log message!");
-                if(balance==0) {
-                    Toast.makeText(getApplicationContext(), "insufficient funds", Toast.LENGTH_SHORT).show();
-                }
+        //Makes play button respond to a click and if the balance is 0 then it says insufficient funds
+        playBTN = findViewById(R.id.playbtn);
+        playBTN.setOnClickListener(view -> {
+            if(balance==0) {
+                Toast.makeText(getApplicationContext(), "insufficient funds", Toast.LENGTH_SHORT).show();
             }
         });
+        //Makes add funds button respond to a click and bring them to Add funds activity
+        addFundsBTN = findViewById(R.id.addFundsBTN);
+        addFundsBTN.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "insufficient funds", Toast.LENGTH_SHORT).show();
+                openAddFundsScreen();
+            }
+        });
+    }
+    public void openAddFundsScreen(){
+        Intent intent = new Intent(this, AddFunds.class);
+        startActivity(intent);
     }
 }
