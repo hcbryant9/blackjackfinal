bceeb45f9be046b57aa8a45922adec7587c9ef3e
hcbryant9 <bryanthc@vcu.edu>
2021-12-07 13:11:02 -0500

    finished the total() method in the Hands class adding a draw method and a clear method. draw increases size and adds a param card to the hand and clear sets the size of the array to zero.

--- app/src/main/java/com/example/blackjackfinal/Hands.java
+++ app/src/main/java/com/example/blackjackfinal/Hands.java
@@ -9,13 +9,29 @@ public class Hands {
     *  a boolean is required to see if their card is an ace (the val is 1)
     * */
     public int total(){
+        int val;
         int total = 0;
         boolean isAce = false;
         /*loop through size of hand adding each card value*/
         for(int i=0; i<size; i++){
-
+            val = hand[i].getVal();
+            if(val > 10) {
+                val = 10;
+            } else if ( val == 1){ /*this means it is an ace*/
+                isAce = true;
+            }
+            total += val;
         }
-        return 0;
+        if (isAce && total + 11 <= 21){ /*if an ace doesn't bust the hand then add 11*/
+            total += 11;
+        }
+        return total;
     }
-
+    public void draw(Card card) {
+        size++;
+        hand[size] = card;
+    }
+    public void clear(){
+        size = 0;
+    }
 }
