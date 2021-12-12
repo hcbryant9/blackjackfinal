5844291783eff5fa6efb2b8fe1c0d20c0930a742
hcbryant9 <bryanthc@vcu.edu>
2021-12-12 13:16:41 -0500

    getting rid of the betting feature until further notice, so I can just use one screen, I also moved the game functionality to a different class called Game that will be called from the main activity as one function call :)

---
+++ app/src/main/java/com/example/blackjackfinal/Game.java
@@ -0,0 +1,61 @@
+package com.example.blackjackfinal;
+
+public class Game {
+    private Deck deck = new Deck();
+    private User user = new User();
+    private Dealer dealer = new Dealer();
+    private Hands dealerHand;
+    private Hands userHand;
+
+    public void winner(Hands user, Hands dealer, boolean dealerBust, boolean userBust) {
+        /*if one person bust or they have the same total*/
+        if (userBust && !dealerBust) {
+            /*dealer win*/
+        } else if (!userBust && dealerBust) {
+            /*user wins*/
+        } else if ((userBust && dealerBust) || (user.total() == dealer.total())) {
+            /*draw*/
+        }
+        if (user.total() > dealer.total()) {
+            /*user win*/
+        } else if (user.total() < dealer.total()) {
+            /*dealer wins*/
+        }
+
+    }
+    public boolean hitOrStand(){
+        /*display hit or stand button
+         * on click make them disappear
+         * store the data
+         * return the input
+         * put the hit button in a while loop
+         * */
+        return true;
+    }
+    public void displayCards(Hands hand){
+        /*display that hand*/
+    }
+    public void blackJack(){
+        /*shuffle the deck and then create two Hands -> one for the user and one for the dealer*/
+        deck.shuffle();
+        dealerHand = new Hands();
+        dealer.dealerDraw(deck.nextCard());
+        dealer.dealerDraw(deck.nextCard());
+        displayCards(dealerHand);
+        userHand = new Hands();
+        user.userDraw(deck.nextCard());
+        user.userDraw(deck.nextCard());
+        /*display the cards -> the dealer than goes first -> then ask user to hit or stand*/
+        displayCards(userHand);
+        dealer.dealerBrain(deck);
+
+        if (hitOrStand()){
+            user.userDraw(deck.nextCard());
+            displayCards(userHand);
+        }
+
+
+        /*logic to see who wins*/
+        winner(userHand,dealerHand,dealer.getDealerBust(),user.getUserBust());
+    }
+}
--- app/src/main/java/com/example/blackjackfinal/MainActivity.java
+++ app/src/main/java/com/example/blackjackfinal/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int balance = 0;
+    /*
+    public static void main(String [] args) {
+        Game newGame = new Game();
+        newGame.blackJack();
+    }
+    */
+
     private Button playBTN;
-    private Button addFundsBTN;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,78 +26,11 @@ public class MainActivity extends AppCompatActivity {
         //Makes play button respond to a click and if the balance is 0 then it says insufficient funds
         playBTN = findViewById(R.id.playbtn);
         playBTN.setOnClickListener(view -> {
-            if(balance==0) {
-                Toast.makeText(getApplicationContext(), "insufficient funds", Toast.LENGTH_SHORT).show();
-            }
+            /*sends user to play screen*/
+            /*plays the game*/
         });
         //Makes add funds button respond to a click and bring them to Add funds activity
-        addFundsBTN = findViewById(R.id.addFundsBTN);
-        addFundsBTN.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "insufficient funds", Toast.LENGTH_SHORT).show();
-                openAddFundsScreen();
-            }
-        });
-    }
-    public void openAddFundsScreen(){
-        Intent intent = new Intent(this, AddFunds.class);
-        startActivity(intent);
-    }
-    private Deck deck = new Deck();
-    private User user = new User();
-    private Dealer dealer = new Dealer();
-    private Hands dealerHand;
-    private Hands userHand;
-    public boolean hitOrStand(){
-        /*display hit or stand button
-        * on click make them disappear
-        * store the data
-        * return the input
-        * put the hit button in a while loop
-        * */
-        return true;
-    }
-    public void displayCards(Hands hand){
-        /*display that hand*/
-    }
-    public void winner(Hands user, Hands dealer, boolean dealerBust, boolean userBust){
-        /*if one person bust or they have the same total*/
-        if(userBust && !dealerBust){
-            /*dealer win*/
-        } else if (!userBust && dealerBust){
-            /*user wins*/
-        } else if ((userBust && dealerBust) ||(user.total()==dealer.total()) ){
-            /*draw*/
-        }
-        if(user.total() > dealer.total()){
-            /*user win*/
-        } else if (user.total() < dealer.total()){
-            /*dealer wins*/
-        }
-
-    }
-    public void game(){
-        /*shuffle the deck and then create two Hands -> one for the user and one for the dealer*/
-        deck.shuffle();
-        dealerHand = new Hands();
-        dealer.dealerDraw(deck.nextCard());
-        dealer.dealerDraw(deck.nextCard());
-        displayCards(dealerHand);
-        userHand = new Hands();
-        user.userDraw(deck.nextCard());
-        user.userDraw(deck.nextCard());
-        /*display the cards -> the dealer than goes first -> then ask user to hit or stand*/
-        displayCards(userHand);
-        dealer.dealerBrain(deck);
-
-        if (hitOrStand()){
-            user.userDraw(deck.nextCard());
-            displayCards(userHand);
-        }
 
 
-        /*logic to see who wins*/
-        winner(userHand,dealerHand,dealer.getDealerBust(),user.getUserBust());
     }
 }
